b3ec322c2a7625ac2a9b02dd18d3fd63ae32010d
Shah Paran Srijan <mspsrijan@gmail.com>
2023-09-17 22:56:27 +0600

    Functionalities to restrict duplicate course addition and limit credit hours

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,27 @@ import { useState } from "react";
 
 function App() {
   const [courseList, setCourseList] = useState([]);
+  const [creditHour, setCreditHour] = useState(0);
+  const [price, setPrice] = useState(0);
 
   function handleAddCourse(course) {
-    const addToCourseList = [...courseList, course];
-    setCourseList(addToCourseList);
+    const courseExists = courseList.some(
+      (existingCourse) => existingCourse.course_title === course.course_title
+    );
+    const newTotalCreditHour = creditHour + course.credit_hour;
+
+    if (!courseExists) {
+      if (newTotalCreditHour <= 20) {
+        const addToCourseList = [...courseList, course];
+        setCourseList(addToCourseList);
+        setCreditHour(newTotalCreditHour);
+        setPrice(price + course.price);
+      } else {
+        alert("Adding this course would exceed 20 credit hours.");
+      }
+    } else {
+      alert("This course is already in your list.");
+    }
   }
 
   return (
@@ -19,7 +36,11 @@ function App() {
           <Courses handleAddCourse={handleAddCourse}></Courses>
         </div>
         <div className="rounded-xl col-span-3">
-          <Summaries courseList={courseList}></Summaries>
+          <Summaries
+            courseList={courseList}
+            creditHour={creditHour}
+            price={price}
+          ></Summaries>
         </div>
       </div>
     </div>
--- src/components/Summaries.jsx
+++ src/components/Summaries.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
-function Summaries({ courseList }) {
-  console.log(courseList);
+function Summaries({ courseList, creditHour, price }) {
   return (
     <div className="bg-white rounded-lg p-5 flex flex-col gap-4">
       <p className="text-[#2F80ED] font-Bold text-lg">
-        Credit Hour Remaining: 7hr
+        Credit Hour Remaining: {20 - creditHour}hr
       </p>
       <hr />
 
@@ -18,15 +18,19 @@ function Summaries({ courseList }) {
         ))}
       </ol>
       <hr />
-      <p className="text-[#444444] font-medium">Total Credit Hour: 13</p>
+      <p className="text-[#444444] font-medium">
+        Total Credit Hour: {creditHour}
+      </p>
       <hr />
-      <p className="text-[#444444] font-semiBold">Total Price: 48000USD</p>
+      <p className="text-[#444444] font-semiBold">Total Price: {price}USD</p>
     </div>
   );
 }
 
 Summaries.propTypes = {
   courseList: PropTypes.array,
+  creditHour: PropTypes.number,
+  price: PropTypes.number,
 };
 
 export default Summaries;
